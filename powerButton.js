814ac7c1863aa495365818c684640d7e24e8bed4
John Jayson B. De Leon <savjaylade84@gmail.com>
2021-03-12 02:01:11 +0800

    add git functionality so i can export to github

---
+++ powerButton.js
@@ -0,0 +1,53 @@
+//Home button
+function NextPage(){
+    window.location.assign("greendayFan.html");
+}
+// go to revolution radio page 
+function rd(){
+    window.location.assign("greendayFan3.html");
+}
+// go to god favorite band page
+function gfb(){
+    window.location.assign("greendayFan_2.html");
+}
+//go to facebook 
+function fmedia(){
+    window.location.assign("https://facebook.com");
+}
+//go to slappy page
+function sl(){
+    window.location.assign("greendayFan4.html");
+}
+//go to slappy page
+function hr(){
+    window.location.assign("greendayFan5.html");
+}
+//go to instagram
+function imedia(){
+    window.location.assign("https://instagram.com");
+}
+// go to twitter page
+function tmedia(){
+    window.location.assign("https://twitter.com");
+}
+// go to google plus page
+function gmedia(){
+    window.location.assign("https://plus.google.com");
+}
+// about button
+function about(){
+    window.location.assign("about.html");
+}
+// lets play now button
+function album(id){
+    id.innerHTML = "Okay Let's play it";
+    window.location.assign("album.html");
+}
+// go to album page
+function Album(){
+    window.location.assign("album.html");
+}
+// go to news update
+function news(){
+    window.location.assign("newsUpdate.html");
+}
